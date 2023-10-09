c165987bb98afd66cf9b46870e6962b3e3460172
mafejjj <mariafernandadejesus28@gmail.com>
2023-10-09 09:09:48 -0300

    Co-authored-by: Marcelo Vinicius Leicht <vlcelo@users.noreply.github.com> Co-authored-by: Tiago Fermiano <tiagofermiano@users.noreply.github.com>

---
+++ php/login.php
@@ -0,0 +1,39 @@
+<?php
+function loginAtendente($cpf, $senha) {
+    // Lembre-se de estabelecer uma conexão com o banco de dados aqui.
+
+    // SQL para selecionar o atendente com base no CPF.
+    $sql = "SELECT id_atendente, senha FROM atendente WHERE cpf = ?";
+
+    // Preparar a declaração SQL.
+    $stmt = $conn->prepare($sql);
+
+    // Bind do parâmetro.
+    $stmt->bind_param("s", $cpf);
+
+    // Executar a declaração preparada.
+    $stmt->execute();
+
+    // Obter o resultado da consulta.
+    $result = $stmt->get_result();
+
+    // Verificar se o atendente com o CPF existe.
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $senhaHash = $row['senha'];
+
+        // Verificar se a senha fornecida corresponde à senha armazenada no banco de dados.
+        if (password_verify($senha, $senhaHash)) {
+            return true; // Sucesso
+        } else {
+            return false; // Senha incorreta
+        }
+    } else {
+        return false; // Atendente não encontrado
+    }
+
+    // Fechar a declaração e a conexão.
+    $stmt->close();
+    $conn->close();
+}
+?>
