74fcf27d5a63e9683196bef9f354229c7411c984
tiagofermiano <tiagomachadofermiano@gmail.com>
2023-10-14 02:24:13 -0300

    Co-authored-by: Maria Fernanda de Jesus <mafejjj@users.noreply.github.com> Co-authored-by: Marcelo Vinicius Leicht <vlcelo@users.noreply.github.com>

--- scripts/index.js
+++ scripts/index.js
@@ -173,8 +173,6 @@ function validarNumeroRGCPFPaciente(input) {
     }
     return null; // Retorna null se o RG não estiver em um formato válido
   }
-  
-  
 
 //função para permitir apenas a escrita de letras no nome do acompanhante
 function validarNomeAcomp(input) {
@@ -228,7 +226,6 @@ function validarNumeroUSB(input) {
         mensagemErro.textContent = "";
     }
 }
-
 
 //funcao para permitir apenas a escrita de numeros no codigo IR
 function validarCodIR(input) {
@@ -296,113 +293,172 @@ function formatarMedidaPressao(input) {
     }
 }
   
-//FUNCAO PARA AUTOCOMPLETAR COM B.C.P.M. E PERMITIR APENAS NÚMEROS
-  function completePulso(inputElement) {
-    const inputValue = inputElement.value;
-    const mensagemErro = document.getElementById("mensagemErro-23");
-    if (/^\d{3}$/.test(inputValue)) {
-        inputElement.value = inputValue + ' B.C.P.M.';
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
+var input = document.getElementById("completePulso");
 
-    } else if (inputValue === ' B.C.P.M.') {
-        // inputElement.value = ''; // Se o usuário inserir apenas '°C', limpe o campo
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (/[^0-9]/.test(inputValue)) {
-        mensagemErro.textContent = "Digite apenas números.";
-        inputElement.value = inputValue.replace(/[^0-9]/g, '');
-    } else {
-        mensagemErro.textContent = "";
+function completePulso(input) {
+    var valor = input.value;
+    var mensagemErro = document.getElementById("mensagemErro-23");
+
+    if (event.inputType === "deleteContentBackward") {
+        input.value = valor.slice(0, -1);
+    }
+    else {
+        var valorNumerico = valor.replace(/\D/g, '');
+
+        if (valorNumerico.length === 2) {
+            mensagemErro.textContent = "";
+        } else if (valorNumerico.length >= 3) {
+            var parte1 = valorNumerico.slice(0, 3);
+            input.value = parte1 + ' B.C.P.M.';
+            mensagemErro.textContent = "";
+        } else {
+            input.value = valorNumerico;
+            mensagemErro.textContent = "Digite apenas números.";
+        }
     }
 }
 
-// FUNÇÃO PARA MRM
-function formatRespiration(inputElement) {
-    const inputValue = inputElement.value;
-    const mensagemErro = document.getElementById("mensagemErro-12");
+var input = document.getElementById("formatRespiration");
 
-    // Verifica se a entrada tem exatamente 2 números
-    if (/^\d{2}$/.test(inputValue)) {
-        inputElement.value = inputValue + ' M.R.M.';
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (inputValue === ' M.R.M.') {
-        // inputElement.value = ''; // Se o usuário inserir apenas '°C', limpe o campo
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (/[^0-9]/.test(inputValue)) {
-        mensagemErro.textContent = "Digite apenas números.";
-        inputElement.value = inputValue.replace(/[^0-9]/g, '');
-    } else {
-        mensagemErro.textContent = "";
+function formatRespiration(input) {
+    var valor = input.value;
+    var mensagemErro = document.getElementById("mensagemErro-12");
+
+    if (event.inputType === "deleteContentBackward") {
+        input.value = valor.slice(0, -1);
+    }
+    else {
+        var valorNumerico = valor.replace(/\D/g, '');
+
+        if (valorNumerico.length === 1) {
+            mensagemErro.textContent = "";
+        } else if (valorNumerico.length >= 2) {
+            var parte1 = valorNumerico.slice(0, 2);
+            input.value = parte1 + ' M.R.M.';
+            mensagemErro.textContent = "";
+        } else {
+            input.value = valorNumerico;
+            mensagemErro.textContent = "Digite apenas números.";
+        }
     }
 }
 
+var input = document.getElementById("completeSaturacao");
 
+function completeSaturacao(input) {
+    var valor = input.value;
+    var mensagemErro = document.getElementById("mensagemErro-22");
 
-function completeSaturacao(inputElement) {
-    const inputValue = inputElement.value;
-    const mensagemErro = document.getElementById("mensagemErro-22");
-  
-    if (/^\d{2}$/.test(inputValue)) {
-        inputElement.value = inputValue + ' %';
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (inputValue === ' %') {
-      // inputElement.value = ''; // Se o usuário inserir apenas ' %', limpe o campo
-      mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (/[^0-9]/.test(inputValue)) {
-        mensagemErro.textContent = "Digite apenas números.";
-        inputElement.value = inputValue.replace(/[^0-9]/g, '');
-    } else {
-        mensagemErro.textContent = "";
+    if (event.inputType === "deleteContentBackward") {
+        input.value = valor.slice(0, -1);
+    }
+    else {
+        var valorNumerico = valor.replace(/\D/g, '');
+
+        if (valorNumerico.length === 1) {
+            mensagemErro.textContent = "";
+        } else if (valorNumerico.length >= 2) {
+            var parte1 = valorNumerico.slice(0, 2);
+            input.value = parte1 + ' %';
+            mensagemErro.textContent = "";
+        } else {
+            input.value = valorNumerico;
+            mensagemErro.textContent = "Digite apenas números.";
+        }
     }
 }
 
-  
+var input = document.getElementById("completeWithHgt");
 
+function completeWithHgt(input) {
+    var valor = input.value;
+    var mensagemErro = document.getElementById("mensagemErro-20");
 
+    if (event.inputType === "deleteContentBackward") {
+        input.value = valor.slice(0, -1);
+    }
+    else {
+        var valorNumerico = valor.replace(/\D/g, '');
 
-
-
-
-
-
-//funcao para permitir que o usuario digite apenas numeros no HGT
-function completeWithHgt(inputElement) {
-    const inputValue = inputElement.value;
-    const mensagemErro = document.getElementById("mensagemErro-20");
-
-    // Verifica se a entrada tem exatamente 2 números
-    if (/^\d{3}$/.test(inputValue)) {
-        inputElement.value = inputValue + ' mg/dL';
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (inputValue === ' mg/dL') {
-        // inputElement.value = ''; // Se o usuário inserir apenas '°C', limpe o campo
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (/[^0-9]/.test(inputValue)) {
-        mensagemErro.textContent = "Digite apenas números.";
-        inputElement.value = inputValue.replace(/[^0-9]/g, '');
-    } else {
-        mensagemErro.textContent = "";
+        if (valorNumerico.length === 2) {
+            mensagemErro.textContent = "";
+        } else if (valorNumerico.length >= 3) {
+            var parte1 = valorNumerico.slice(0, 3);
+            input.value = parte1 + ' mg/dL';
+            mensagemErro.textContent = "";
+        } else {
+            input.value = valorNumerico;
+            mensagemErro.textContent = "Digite apenas números.";
+        }
     }
 }
 
- // Função para completar com "°C" após o usuário digitar dois números
- function completeWithDegreeC(inputElement) {
-    const inputValue = inputElement.value;
-    const mensagemErro = document.getElementById("mensagemErro-21");
+var input = document.getElementById("completeWithDegreeC");
 
-    // Verifica se a entrada tem exatamente 2 números
-    if (/^\d{2}$/.test(inputValue)) {
-        inputElement.value = inputValue + '°C';
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (inputValue === '°C') {
-        inputElement.value = ''; // Se o usuário inserir apenas '°C', limpe o campo
-        mensagemErro.textContent = ""; // Limpa a mensagem de erro
-    } else if (/[^0-9]/.test(inputValue)) {
-        mensagemErro.textContent = "Digite apenas números.";
-        inputElement.value = inputValue.replace(/[^0-9]/g, '');
-    } else {
-        mensagemErro.textContent = "";
+function completeWithDegreeC(input) {
+    var valor = input.value;
+    var mensagemErro = document.getElementById("mensagemErro-21");
+
+    if (event.inputType === "deleteContentBackward") {
+        input.value = valor.slice(0, -1);
+    }
+    else {
+        var valorNumerico = valor.replace(/\D/g, '');
+
+        if (valorNumerico.length === 1) {
+            mensagemErro.textContent = "";
+        } else if (valorNumerico.length >= 2) {
+            var parte1 = valorNumerico.slice(0, 2);
+            input.value = parte1 + ' °C';
+            mensagemErro.textContent = "";
+        } else {
+            input.value = valorNumerico;
+            mensagemErro.textContent = "Digite apenas números.";
+        }
     }
 }
+
+// checkbox > 2 seg e 2 seg
+
+const scalesCheckbox = document.getElementById('scales');
+const hornsCheckbox = document.getElementById('horns');
+
+scalesCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        hornsCheckbox.disabled = true;
+    } else {
+        hornsCheckbox.disabled = false;
+    }
+});
+
+hornsCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        scalesCheckbox.disabled = true;
+    } else {
+        scalesCheckbox.disabled = false;
+    }
+});
+
+// checkbox NORMAL e ANORMAL
+
+const normalscalesCheckbox = document.getElementById('normal');
+const anormalhornsCheckbox = document.getElementById('anormal');
+
+normalscalesCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        anormalhornsCheckbox.disabled = true;
+    } else {
+        anormalhornsCheckbox.disabled = false;
+    }
+});
+
+anormalhornsCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+        normalscalesCheckbox.disabled = true;
+    } else {
+        normalscalesCheckbox.disabled = false;
+    }
+});
 
 const inputElement = document.getElementById("username");
 
@@ -518,7 +574,6 @@ function toggleSamu() {
     });
 }
 
-
 function toggleInputCit() {
     var checkbox = document.getElementById("myCheckbox");
     var input = document.getElementById("myInput");
@@ -530,16 +585,155 @@ function toggleInputCit() {
     }
 }
 
-
-function toggleCheckboxes(checkbox) {
-    var internaCheckbox = document.getElementById("horns");
-    var externaCheckbox = document.getElementById("horns2");
+function toggleHemorragia(checkbox) {
+    var checkboxesInternas = document.querySelectorAll('.sub-checkbox-resp.interna');
+    var checkboxesExternas = document.querySelectorAll('.sub-checkbox-resp.externa');
 
     if (checkbox.checked) {
-        internaCheckbox.disabled = false;
-        externaCheckbox.disabled = false;
+        checkboxesInternas.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesExternas.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
     } else {
-        internaCheckbox.disabled = true;
-        externaCheckbox.disabled = true;
+        checkboxesInternas.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesExternas.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
     }
 }
+
+function toggleEdema(checkbox) {
+    var checkboxesGeneralizado = document.querySelectorAll('.sub-checkbox-resp.generalizado');
+    var checkboxesLocalizado = document.querySelectorAll('.sub-checkbox-resp.localizado');
+
+    if (checkbox.checked) {
+        checkboxesGeneralizado.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesLocalizado.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+    } else {
+        checkboxesGeneralizado.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesLocalizado.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
+    }
+}
+
+function toggleParada(checkbox) {
+    var checkboxesCardiaca = document.querySelectorAll('.sub-checkbox-resp.cardiaca');
+    var checkboxesRespiratoria = document.querySelectorAll('.sub-checkbox-resp.respiratoria');
+
+    if (checkbox.checked) {
+        checkboxesCardiaca.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesRespiratoria.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+    } else {
+        checkboxesCardiaca.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesRespiratoria.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
+    }
+}
+
+function toggleCianose(checkbox) {
+    var checkboxesLabios = document.querySelectorAll('.sub-checkbox-resp.labios');
+    var checkboxesExtremidade = document.querySelectorAll('.sub-checkbox-resp.extremidade');
+
+    if (checkbox.checked) {
+        checkboxesLabios.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesExtremidade.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+    } else {
+        checkboxesLabios.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesExtremidade.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
+    }
+}
+
+function togglePupilas(checkbox) {
+    var checkboxesAnisocoria = document.querySelectorAll('.sub-checkbox-resp.anisocoria');
+    var checkboxesIsocoria = document.querySelectorAll('.sub-checkbox-resp.isocoria');
+    var checkboxesMidriase = document.querySelectorAll('.sub-checkbox-resp.midriase');
+    var checkboxesMiose = document.querySelectorAll('.sub-checkbox-resp.miose');
+    var checkboxesReagente = document.querySelectorAll('.sub-checkbox-resp.reagente');
+    var checkboxesNaoReagente = document.querySelectorAll('.sub-checkbox-resp.naoreagente');
+
+    if (checkbox.checked) {
+        checkboxesAnisocoria.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesIsocoria.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesMidriase.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesMiose.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesReagente.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+        checkboxesNaoReagente.forEach(function (cb) {
+            cb.removeAttribute('disabled');
+        });
+    } else {
+        checkboxesAnisocoria.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesIsocoria.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
+        checkboxesMidriase.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesMiose.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
+        checkboxesReagente.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox interna
+        });
+        checkboxesNaoReagente.forEach(function (cb) {
+            cb.setAttribute('disabled', 'disabled');
+            cb.checked = false; // Desmarca a checkbox externa
+        });
+    }
+}
+
+
+
+
+
+
